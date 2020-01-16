6f534dbfc06c8327c0056e7fd028e603080b85be
Paulo R. Batalhão <paulo@javahub.dev>
2020-01-15 23:36:15 -0300

    Persistência de um novo Livro.

--- src/main/java/br/com/batalhao/socialbooks/application/ClientApplication.java
+++ src/main/java/br/com/batalhao/socialbooks/application/ClientApplication.java
@@ -8,16 +8,23 @@ import br.com.batalhao.socialbooks.client.domain.Livro;
 public class ClientApplication {
 
 	public static void main(String[] args) {
-		LivrosClient livrosClient = new LivrosClient();
+		LivrosClient livrosClient = new LivrosClient("http://localhost:8080", "user", "user");
+
+		livrosClient.list().stream().forEach(record -> System.out.println(record.getNome()));
+
+		System.out.println("====================");
 
 		Livro livro = new Livro();
-		livro.setNome("Java Completo e Total");
+		livro.setNome("Java Completo e Total 2");
 		livro.setEditora("Nova Brasil");
 		livro.setPublicacao(LocalDate.of(2017, 10, 05));
 		livro.setNumeroPaginas(300);
-		System.out.println(livrosClient.save(livro));
+		String uriRetorno = livrosClient.save(livro);
+		System.out.println(uriRetorno);
 
-		livrosClient.list().stream().forEach(record -> System.out.println(record.getNome()));
+//		System.out.println("====================");
+
+		System.out.println(livrosClient.find(uriRetorno).getNome());
 	}
 
 }
--- src/main/java/br/com/batalhao/socialbooks/client/LivrosClient.java
+++ src/main/java/br/com/batalhao/socialbooks/client/LivrosClient.java
@@ -2,6 +2,7 @@ package br.com.batalhao.socialbooks.client;
 
 import java.net.URI;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 import org.springframework.http.RequestEntity;
@@ -12,37 +13,43 @@ import br.com.batalhao.socialbooks.client.domain.Livro;
 
 public class LivrosClient {
 
-	public List<Livro> list() {
-		RestTemplate restTemplate = new RestTemplate();
+	private RestTemplate restTemplate;
 
-		RequestEntity<Void> request = RequestEntity.get(URI.create("http://localhost:8080/livros"))
-				.header("Authorization", "Basic dXNlcjp1c2Vy").build();
+	private String URI_BASE;
+
+	private String URN_BASE = "/livros";
+
+	private String credencial;
+
+	public LivrosClient(String url, String usuario, String senha) {
+		restTemplate = new RestTemplate();
+		URI_BASE = url.concat(URN_BASE);
+		String credencialAux = usuario + ":" + senha;
+		credencial = "Basic " + Base64.getEncoder().encodeToString(credencialAux.getBytes());
+	}
+
+	public List<Livro> list() {
+		RequestEntity<Void> request;
+		request = RequestEntity.get(URI.create(URI_BASE)).header("Authorization", credencial).build();
 
 		ResponseEntity<Livro[]> response = restTemplate.exchange(request, Livro[].class);
-
 		return Arrays.asList(response.getBody());
 	}
 
-//	public Livro find(Long livroId) {
-//		RestTemplate restTemplate = new RestTemplate();
-//
-//		RequestEntity<Void> request = RequestEntity.get(URI.create("http://localhost:8080/livros/" + livroId))
-//				.header("Authorization", "Basic dXNlcjp1c2Vy").build();
-//
-//		ResponseEntity<Livro[]> response = restTemplate.exchange(request, Livro[].class);
-//
-//		return Arrays.asList(response.getBody()).stream().findFirst().orElse(null);
-//	}
-
 	public String save(Livro livro) {
-		RestTemplate restTemplate = new RestTemplate();
-
-		RequestEntity<Livro> request = RequestEntity.post(URI.create("http://localhost:8080/livros"))
-				.header("Authorization", "Basic dXNlcjp1c2Vy").body(livro);
+		RequestEntity<Livro> request;
+		request = RequestEntity.post(URI.create(URI_BASE)).header("Authorization", credencial).body(livro);
 
 		ResponseEntity<Void> response = restTemplate.exchange(request, Void.class);
-
 		return response.getHeaders().getLocation().toString();
+	}
+
+	public Livro find(String uri) {
+		RequestEntity<Void> request;
+		request = RequestEntity.get(URI.create(uri)).header("Authorization", credencial).build();
+
+		ResponseEntity<Livro> response = restTemplate.exchange(request, Livro.class);
+		return response.getBody();
 	}
 
 }
